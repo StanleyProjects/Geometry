76ff8ab951752b97377bc58742ee0477d4c3cadd
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 01:17:52 +0300

    matrix mut operators

--- lib/src/main/kotlin/sp/kx/geometry/MutableMatrix.kt
+++ lib/src/main/kotlin/sp/kx/geometry/MutableMatrix.kt
@@ -37,4 +37,31 @@ class MutableMatrix(
             m03, m13, m23, m33,
         )
     }
+
+    operator fun timesAssign(other: Matrix) {
+        val m00 = this.m00 * other.m00 + this.m01 * other.m10 + this.m02 * other.m20 + this.m03 * other.m30
+        val m01 = this.m00 * other.m01 + this.m01 * other.m11 + this.m02 * other.m21 + this.m03 * other.m31
+        val m02 = this.m00 * other.m02 + this.m01 * other.m12 + this.m02 * other.m22 + this.m03 * other.m32
+        val m03 = this.m00 * other.m03 + this.m01 * other.m13 + this.m02 * other.m23 + this.m03 * other.m33
+        //
+        val m10 = this.m10 * other.m00 + this.m11 * other.m10 + this.m12 * other.m20 + this.m13 * other.m30
+        val m11 = this.m10 * other.m01 + this.m11 * other.m11 + this.m12 * other.m21 + this.m13 * other.m31
+        val m12 = this.m10 * other.m02 + this.m11 * other.m12 + this.m12 * other.m22 + this.m13 * other.m32
+        val m13 = this.m10 * other.m03 + this.m11 * other.m13 + this.m12 * other.m23 + this.m13 * other.m33
+        //
+        val m20 = this.m20 * other.m00 + this.m21 * other.m10 + this.m22 * other.m20 + this.m23 * other.m30
+        val m21 = this.m20 * other.m01 + this.m21 * other.m11 + this.m22 * other.m21 + this.m23 * other.m31
+        val m22 = this.m20 * other.m02 + this.m21 * other.m12 + this.m22 * other.m22 + this.m23 * other.m32
+        val m23 = this.m20 * other.m03 + this.m21 * other.m13 + this.m22 * other.m23 + this.m23 * other.m33
+        //
+        val m30 = this.m30 * other.m00 + this.m31 * other.m10 + this.m32 * other.m20 + this.m33 * other.m30
+        val m31 = this.m30 * other.m01 + this.m31 * other.m11 + this.m32 * other.m21 + this.m33 * other.m31
+        val m32 = this.m30 * other.m02 + this.m31 * other.m12 + this.m32 * other.m22 + this.m33 * other.m32
+        val m33 = this.m30 * other.m03 + this.m31 * other.m13 + this.m32 * other.m23 + this.m33 * other.m33
+        //
+        this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03
+        this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13
+        this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23
+        this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33
+    }
 }
