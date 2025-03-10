96054df2cb963d8c6e0434ec2c109da7fada1af4
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 01:12:20 +0300

    rotation mut

---
+++ lib/src/main/kotlin/sp/kx/geometry/MutableRotation.kt
@@ -0,0 +1,20 @@
+package sp.kx.geometry
+
+import java.util.Objects
+
+class MutableRotation(
+    override var aX: Double,
+    override var aY: Double,
+    override var aZ: Double,
+) : Rotation {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Rotation -> aX == other.aX && aY == other.aY && aZ == other.aZ
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(aX, aY, aZ)
+    }
+}
